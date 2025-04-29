71b6ce08e574aaac409070dfbcf662612b1c94c0
Saschaski11 <159729404+Saschaski11@users.noreply.github.com>
2025-04-29 18:03:00 +0200

    Update Coldwater.js

--- Coldwater.js
+++ Coldwater.js
@@ -2,10 +2,12 @@ elements.cold_water = {
     color: "#66ccff",
     behavior: behaviors.LIQUID,
     temp: 0,
-    category: "liquids",
+    category: "w",
     state: "liquid",
     density: 1000,
     tick: function(pixel) {
         pixel.temp = 0;
     }
 };
+
+if (!elementCategories.includes("w")) elementCategories.push("w");
